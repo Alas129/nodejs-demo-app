1b1395f3e9998a63acb68f38403492eb5d09be47
alas129 <alas129@163.com>
2024-07-16 00:57:27 -0700

    minimal update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // We'll use our routes function that we setup and imported above
 // and then pass it to our app Express 
 routes(app);
-// Setting the server to listen at port 3000
+// Setting the server to listen at port 3000.
 app.listen(PORT, HOST, function () {
   console.log(`Server started on http://${HOST}:${PORT}`);
 });
